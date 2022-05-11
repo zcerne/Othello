917e130ab3f2dd22f32009e295fc5d80cebdfd83
eagul <jan.martin.jamnik@gmail.com>
2022-05-11 10:02:01 +0200

    List ni List ampak Veja

---
+++ Othello/src/inteligenca/Veja.java
@@ -0,0 +1,18 @@
+package inteligenca;
+
+import logika.Igra;
+
+public class Veja {
+	
+	private Igra igra;
+	private double wins;
+	private int visits;
+
+	public List(Igra igra, int visits, double wins) {
+		this.igra = igra;
+		this.visits = visits;
+		this.wins = wins;
+		
+		
+	}
+}
